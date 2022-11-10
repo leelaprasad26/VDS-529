ecafe77acfde9b875a824ee69af544aaa14b101c
Madhumitha Rajarajan <99444469+maddieraj@users.noreply.github.com>
2022-11-09 23:40:06 -0600

    Updated second graph

--- scattorPlot.js
+++ scattorPlot.js
@@ -32,4 +32,35 @@ function scatter(class1,class2,color0,color1,valuesWithTopics0,valuesWithTopics1
         value: valuesWithTopics1[topicKey]
       });
     }
+// append the bar rectangles to the svg element
+    console.log(data1)//1. 20 ->55 55 ->90 2 115
+    svg.selectAll("rect1")
+      .data(data1)
+      .join("rect")
+      .attr("x", 1)
+      
+      .attr("id",function (d){return "data1"+d.key})
+      .attr("transform", function (d) { console.log(d.value); return `translate(${x(d.key)+10}, ${y(d.value)})` })
+      .attr("width", function (d) { return 30 })
+      .attr("height", function (d) { return height - y(d.value); })
+      .style("fill", color1)
+
+      var tooltip2 = d3.select("#div_customContent")
+      .append("div")
+          .style("position", "absolute")
+          .style("visibility", "hidden")
+          .style("background-color", "white")
+          .style("border", "solid")
+          .style("border-width", "1px")
+          .style("border-radius", "5px")
+          .style("padding", "10px")
+          .html("<p>I'm a tooltip written in HTML</p><img src='https://github.com/holtzy/D3-graph-gallery/blob/master/img/section/ArcSmal.png?raw=true'></img><br>Fancy<br><span style='font-size: 40px;'>Isn't it?</span>");
+
     
+          var chartData = [
+            {name: "Max Value", color: "#af6d00"},
+            {name: "Avarage Value", color: "#007f00"},
+            {name: "Min Value", color: "#0000a8"},
+       
+           ];
+        
